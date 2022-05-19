905ab6b67869a28cce93ecb7e3b2bd3fc72e4b7a
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2022-05-19 13:33:18 +1000

    Update loader.server.lua

--- loader/loader.server.lua
+++ loader/loader.server.lua
@@ -10,7 +10,7 @@ local channels = { --other channels coming soon
 --!!! YOU MUST HAVE THE MAINMODULE YOU WANT TO USE IN YOUR INVENTORY TO USE IT
 
 local UPDATE_CHANNEL = channels["STABLE"]  --options for the update channel: this is the main version "STABLE" this is the version i use for features that currently don't work well "UNSTABLE" this is what i use for prototyping ideas for fastr "EXPERIMENTAL" if you get a warning saying that your loader version is too old then switch your update channel to "COMPATABILITY"
-local LoaderVersion = "0.1.0" --if you change this it could stop fastr to stop working for you. this is used to check the compatability of your version of the loader and the latest version of
+local LoaderVersion = "0.2.0" --if you change this it could stop fastr to stop working for you. this is used to check the compatability of your version of the loader and the latest version of
 local ModuleVersion = InsertService:GetLatestAssetVersionAsync(UPDATE_CHANNEL) -- change this to a different version of Fastr if you want. If you keep it the same you will receive automatic updates 
 
 local Fastr = InsertService:LoadAsset(UPDATE_CHANNEL)
@@ -20,4 +20,3 @@ local module = require(Fastr)
 module:init(script.Parent, LoaderVersion)
 
 script.Parent:Destroy()
---0.66
